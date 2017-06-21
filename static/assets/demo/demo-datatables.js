d4ae7059bfaee8a4d70641790cd15ffa1787b7b1
0sw4l <ioswxd@gmail.com>
2017-06-21 00:25:45 -0500

    assets and frontend ... "ready", now working

--- static/assets/demo/demo-datatables.js
+++ static/assets/demo/demo-datatables.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $('#example').dataTable({
     	"language": {
     		"lengthMenu": "_MENU_",
-			"zeroRecords": "Lo sentimos, no se encontraron registros",
+			"zeroRecords": "Lo sentimos, no se encontraron registros para mostrar",
             "info": "Mostrando pagina _PAGE_ de _PAGES_",
             "infoEmpty": "Ho hay registros disponibles",
             "infoFiltered": "(filtrado de _MAX_ registros)",
@@ -14,9 +14,19 @@ $(document).ready(function() {
     			"previous": "Anterior",
 				"next": "Siguiente"
 			}
-    	}
+    	},
+		"dom": "Bfrtip",
+   		"bProcessing": true,
+   		"bAutoWidth": false,
+   		"responsive": true,
+   		"buttons": [
+			{ extend: 'copy', text: 'Copiar <i class="fa fa-copy"></i>'},
+			{ extend: 'excel', text: 'Exportar a Excel <i class="fa fa-file-excel-o"></i>'},
+			{ extend: 'pdf', text: 'Exportar a PDF <i class="fa fa-file-pdf-o"></i>'},
+			{ extend: 'colvis', text: 'Columnas Visibles <i class="fa fa-columns"></i>'},
+			{ extend: 'print', text: 'Imprimir <i class="fa fa-print"></i>'}
+		]
     });
-    $('.dataTables_filter input').attr('placeholder','Buscar...');
 
 
     //DOM Manipulation to move datatable elements integrate to panel
