67bceece452c450ee6ec302132d96b8487761bbf
DiegoVadx <diego.v.exe@gmail.com>
2017-06-21 14:49:01 -0500

    Changes Create Capacitacion CRUD - Habilidades Blandas CRUD

--- apps/habilidades_blandas/urls.py
+++ apps/habilidades_blandas/urls.py
@@ -1,5 +1,30 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+
+    url(r'^crear-capacitacion/',
+        views.CapacitacionCreateView.as_view(),
+        name='crear_capacitacion'),
+
+    url(r'^editar-capacitacion/',
+        views.CapacitacionUpdateView.as_view(),
+        name='editar_capacitacion'),
+
+    url(r'^lista-capacitacion/',
+        views.CapacitacionListView.as_view(),
+        name='lista_capacitacion'),
+
+    url(r'^crear-habilidades-blandas/',
+        views.HabilidadesBlandasCreateView.as_view(),
+        name='crear_habilidades_blandas'),
+
+    url(r'^editar-habilidades-blandas/',
+        views.HabilidadesBlandasUpdateView.as_view(),
+        name='editar_habilidades_blandas'),
+
+    url(r'^list-habilidades-blandas',
+        views.HabilidadesBlandasListView.as_view(),
+        name='lista_habilidades_blandas'),
 
 ]
--- apps/habilidades_blandas/views.py
+++ apps/habilidades_blandas/views.py
@@ -1,3 +1,46 @@
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
+from . import models
+from . import forms
+from apps.utils import views
 
-# Create your views here.
+class CapacitacionBaseView(object):
+    model = models.Capacitacion
+    form_class = forms.CapacitacionForm
+    success_url = reverse_lazy('habilidades_blandas:lista_capacitacion')
+
+
+class CapacitacionCreateView(CapacitacionBaseView, views.BaseCreateView):
+    pass
+
+
+class CapacitacionUpdateView(CapacitacionBaseView, views.BaseUpdateView):
+    pass
+
+
+class CapacitacionListView(views.BaseListViewDinamicHeader):
+    HEADER = ('id', 'Nombre')
+    model = models.Capacitacion
+    template_name = 'apps/habilidades_blandas/capacitacion_list.html'
+
+
+class HabilidadesBlandasBaseView(object):
+    model = models.HabilidadBlanda
+    form_class = forms.HabilidadBlandaForm
+    success_url = reverse_lazy('habilidades_blandas:lista_habilidades_blandas')
+
+
+class HabilidadesBlandasCreateView(HabilidadesBlandasBaseView, views.BaseCreateView):
+    pass
+
+
+class HabilidadesBlandasUpdateView(HabilidadesBlandasBaseView, views.BaseUpdateView):
+    pass
+
+
+class HabilidadesBlandasListView(views.BaseListViewDinamicHeader):
+    HEADER = ('Persona', 'Capacitacion', 'Estado Certificado', 'Tipo Alerta', 'Test', 'Observaciones')
+    model = models.HabilidadBlanda
+    template_name = 'apps/habilidades_blandas/habilidades_blandas_list.html'
+
+
